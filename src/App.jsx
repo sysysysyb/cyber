a4a239b121dcc4085e78a7c8ab5dff8775f7d489
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-14 18:02:01 +0900

    ✨ Feat: App.jsx에 MainLayout, Home 기본 라우팅 설정 추가 (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import MainLayout from '@layouts/MainLayout';
+import Home from '@pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
