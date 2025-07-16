54b4d1410cc1fe337f9cafa8deb7135ac024cbb7
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-16 22:04:17 +0900

    🚑️ Hotfix: HeaderMenu JSX 닫는 태그 추가 (#12)

--- src/components/header/HeaderMenu.jsx
+++ src/components/header/HeaderMenu.jsx
@@ -9,6 +9,7 @@ function HeaderMenu() {
       <S.MainNavItem to="/about">About</S.MainNavItem>
       <S.MainNavItem to="/contact-us">Contact us</S.MainNavItem>
       <S.MainNavItem to="/blog">Blog</S.MainNavItem>
+    </S.MainNav>
   );
 }
 
