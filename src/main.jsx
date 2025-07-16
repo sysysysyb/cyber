bcb96e2d46289a8eccf796510ef89dbbc67ecbb9
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-17 04:05:25 +0900

    🔥 Remove: main.jsx의 Strict 모드 삭제 (#13)
    api 요청 테스트를 원활히 하기 위해 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import GlobalStyles from '@styles/GlobalStyles.jsx';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <GlobalStyles />
     <App />
-  </StrictMode>
+  </>
 );
-
