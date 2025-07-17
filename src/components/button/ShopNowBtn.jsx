a8a5bc26ceb7007be9ab5a4dea3561ad111e90d9
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-17 19:00:57 +0900

    ♻️ Ref: ShopNow 버튼 컴포넌트 스타일 코드 분리 (#13)

--- src/components/button/ShopNowBtn.jsx
+++ src/components/button/ShopNowBtn.jsx
@@ -1,25 +1,4 @@
-import styled from '@emotion/styled';
-import { Link } from 'react-router';
-
-const ShopNowBtnContainer = styled.div`
-  padding: 1rem 3.5rem;
-  border: 1px solid ${(props) => props.theme.textColor || '#000'};
-  color: ${(props) => props.theme.textColor || '#000'};
-  font-weight: 500;
-  border-radius: 0.375rem;
-  transition: 0.2s ease-out;
-
-  &:hover {
-    background-color: ${(props) => props.theme.textColor || '#000'};
-    color: ${(props) => props.theme.bgColor || '#fff'};
-  }
-`;
-
-const ShopNowBtnLink = styled(Link)`
-  width: fit-content;
-  height: fit-content;
-  text-decoration: none;
-`;
+import { ShopNowBtnContainer, ShopNowBtnLink } from './styles';
 
 function ShopNowBtn({ path, theme }) {
   return (
---
+++ src/components/button/styles.jsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router';
+
+export const ShopNowBtnContainer = styled.div`
+  padding: 1rem 3.5rem;
+  border: 1px solid ${(props) => props.theme.textColor || '#000'};
+  color: ${(props) => props.theme.textColor || '#000'};
+  font-weight: 500;
+  border-radius: 0.375rem;
+  transition: 0.2s ease-out;
+
+  &:hover {
+    background-color: ${(props) => props.theme.textColor || '#000'};
+    color: ${(props) => props.theme.bgColor || '#fff'};
+  }
+`;
+
+export const ShopNowBtnLink = styled(Link)`
+  width: fit-content;
+  height: fit-content;
+  text-decoration: none;
+`;
