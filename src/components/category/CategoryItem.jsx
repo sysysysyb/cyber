b33cbeb362d0cae519c28afc00c3aa911ec79b17
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-07-22 02:14:16 +0900

    ✨ Feat: 카테고리 item 컴포넌트 추가 (#16)

---
+++ src/components/category/CategoryItem.jsx
@@ -0,0 +1,37 @@
+import FavoritesIcon from '@assets/icons/header/favorites-icon.svg?react';
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router';
+
+export const CategoryItemContainer = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  min-width: 10rem;
+  border: 0;
+  padding: 1.5rem;
+  background-color: #ededed;
+  aspect-ratio: 4/3;
+  border-radius: 1rem;
+  cursor: pointer;
+`;
+
+export const CategoryItemText = styled.span`
+  font-size: 1.125rem;
+  font-weight: 500;
+`;
+
+function CategoryItem({ category }) {
+  const { id, icon, name } = category;
+  const navigate = useNavigate();
+
+  return (
+    <CategoryItemContainer onClick={() => navigate(`/category/${id}`)}>
+      {icon ? <img src={icon} alt={name} /> : <FavoritesIcon />}
+      <CategoryItemText>{name}</CategoryItemText>
+    </CategoryItemContainer>
+  );
+}
+
+export default CategoryItem;
